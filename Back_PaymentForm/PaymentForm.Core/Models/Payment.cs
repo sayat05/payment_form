432ee41d70462b01eaec446ef980f538d1a2ff61
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-15 22:16:50 +0500

    #1
    1) Добавил сущноти платежа и пользователя

---
+++ Back_PaymentForm/PaymentForm.Core/Models/Payment.cs
@@ -0,0 +1,15 @@
+using PaymentForm.Core.Enums;
+
+namespace PaymentForm.Core.Models;
+
+public class Payment
+{
+    public long Id { get; init; }
+    public long UserId { get; init; }
+    public required string Email { get; init; }
+    public decimal Amount { get; init; }
+    public CurrencyType Currency { get; init; }
+    public PaymentStatus Status { get; init; }
+    public string Comment { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; init; }
+}
---
+++ Back_PaymentForm/PaymentForm.Core/Models/User.cs
@@ -0,0 +1,10 @@
+namespace PaymentForm.Core.Models;
+
+public class User
+{
+    public long Id { get; init; }
+    public required string Name { get; set; }
+    public required string Email { get; init; }
+    public string PhoneNumber { get; set; } = string.Empty;
+    public required string WalletNumber { get; init; }
+}
