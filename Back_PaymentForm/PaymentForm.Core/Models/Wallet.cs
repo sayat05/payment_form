92a4fe812702a68771b85aed7a37300e407aaecf
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 01:13:06 +0500

    #9

--- Back_PaymentForm/PaymentForm.Core/Models/Wallet.cs
+++ Back_PaymentForm/PaymentForm.Core/Models/Wallet.cs
@@ -2,7 +2,7 @@ namespace PaymentForm.Core.Models;
 
 public class Wallet
 {
-    public long Id { get; set; }
+    public long Id { get; init; }
     public required string WalletNumber { get; init; }
     public required decimal Balance { get; init; }
     public required long UserId { get; init; }
