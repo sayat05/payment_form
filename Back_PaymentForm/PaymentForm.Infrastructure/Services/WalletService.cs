4f67d9d392d6f464fa5eeea3938b092cc3a3110b
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 01:14:45 +0500

    #2
    1) Добавил сервис кошелька

---
+++ Back_PaymentForm/PaymentForm.Infrastructure/Services/WalletService.cs
@@ -0,0 +1,39 @@
+﻿using PaymentForm.Core.Abstractions.IRepositories;
+using PaymentForm.Core.Abstractions.IServices;
+using PaymentForm.Core.DtoModels;
+using PaymentForm.Core.Models;
+
+namespace PaymentForm.Infrastructure.Services;
+
+public class WalletService(IWalletRepository repository) : IWalletService
+{
+    public async Task<IEnumerable<Wallet>> GetAll()
+    {
+        return await repository.GetAll();
+    }
+
+    public async Task<IEnumerable<Wallet>> GetByUserId(long userId)
+    {
+        return await repository.GetByUserId(userId);
+    }
+
+    public async Task<Wallet?> GetById(long id)
+    {
+        return await repository.GetById(id);
+    }
+
+    public async Task<Wallet?> GetWalletNumber(string walletNumber)
+    {
+        return await repository.GetWalletNumber(walletNumber);
+    }
+
+    public async Task<long> Add(WalletAddDto dto)
+    {
+        return await repository.Add(new Wallet
+        {
+            WalletNumber = dto.WalletNumber,
+            Balance = dto.Balance,
+            UserId = dto.UserId
+        });
+    }
+}
