495d7d15a0d1e1ccc19daac342ab34c385f5d39c
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-20 21:43:24 +0500

    #11
    1) Добавил контроллеры и postrgreSql DI

--- Back_PaymentForm/PaymentForm.Web/Program.cs
+++ Back_PaymentForm/PaymentForm.Web/Program.cs
@@ -1,8 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentForm.DataBase.DataBase;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+#region Swagger
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#endregion
+
+#region Controllers
+
+builder.Services.AddControllers();
+
+#endregion
+
+#region EfCore
+
+builder.Services.AddDbContext<MyAppContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
+
 
 var app = builder.Build();
 
@@ -13,5 +35,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+app.MapControllers();
 app.Run();
