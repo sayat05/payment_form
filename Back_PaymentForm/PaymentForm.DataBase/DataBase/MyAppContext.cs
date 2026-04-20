88c74560a3628da1287fe4541e6e0eab42c7cd28
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-20 20:44:31 +0500

    #4
    1) Настроил MyApp контекст для подключения "таблиц" и их конфигураций

--- Back_PaymentForm/PaymentForm.DataBase/DataBase/MyAppContext.cs
+++ Back_PaymentForm/PaymentForm.DataBase/DataBase/MyAppContext.cs
@@ -1,6 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using PaymentForm.DataBase.EfCoreModels;
+
 namespace PaymentForm.DataBase.DataBase;
 
-public class MyAppContext
+public class MyAppContext(DbContextOptions<MyAppContext> options) : DbContext(options)
 {
-    
+    private DbSet<UserEfCore> Users => Set<UserEfCore>();
+    private DbSet<PaymentEfCore> Payments => Set<PaymentEfCore>();
+    private DbSet<WalletEfCore> Wallets => Set<WalletEfCore>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyAppContext).Assembly);
+    }
 }
