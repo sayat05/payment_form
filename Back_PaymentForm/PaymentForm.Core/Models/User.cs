c26fef748e4c73f32a1b51a6d59f65bb2cea9422
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 01:46:17 +0500

    #9

--- Back_PaymentForm/PaymentForm.Core/Models/User.cs
+++ Back_PaymentForm/PaymentForm.Core/Models/User.cs
@@ -3,7 +3,7 @@ namespace PaymentForm.Core.Models;
 public class User
 {
     public long Id { get; init; }
-    public required string Name { get; set; }
+    public required string Name { get; init; }
     public required string Email { get; init; }
     public string PhoneNumber { get; set; } = string.Empty;
 }
