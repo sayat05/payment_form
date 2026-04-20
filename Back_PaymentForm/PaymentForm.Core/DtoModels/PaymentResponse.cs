4c97088940b15b17de44c94feebc7ecfcb4b824a
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-20 23:04:22 +0500

    #1
    1) Добавил dto модель для пользвователей

---
+++ Back_PaymentForm/PaymentForm.Core/DtoModels/PaymentResponse.cs
@@ -0,0 +1,13 @@
+namespace PaymentForm.Core.DtoModels;
+
+public class PaymentResponse
+{
+    public long Id { get; init; }
+    public long WalletId { get; init; }
+    public required string Email { get; init; }
+    public decimal Amount { get; init; }
+    public required string Currency { get; init; }
+    public required string Status { get; init; }
+    public string Comment { get; set; } = string.Empty;
+    public required string CreatedAt { get; init; }
+}
