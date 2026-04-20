04ca881581b212be1257217119e4a7d5f287d5fe
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-20 20:38:20 +0500

    #9
    1) Указал явно null давая компилятору знать что я в теме

--- Back_PaymentForm/PaymentForm.DataBase/EfCoreModels/PaymentEfCore.cs
+++ Back_PaymentForm/PaymentForm.DataBase/EfCoreModels/PaymentEfCore.cs
@@ -1,4 +1,3 @@
-using System.Security.AccessControl;
 using PaymentForm.Core.Enums;
 
 namespace PaymentForm.DataBase.EfCoreModels;
@@ -7,16 +6,11 @@ public class PaymentEfCore
 {
     public long Id { get; init; }
     public long WalletId { get; init; }
-    public WalletEfCore WalletEfCore { get; init; }
+    public WalletEfCore WalletEfCore { get; init; } = null!;
     public required string Email { get; init; }
     public decimal Amount { get; init; }
     public CurrencyType Currency { get; init; }
     public PaymentStatus Status { get; init; }
     public string Comment { get; set; } = string.Empty;
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
-
-    /// <summary>
-    /// У одного платежа много счетов
-    /// </summary>
-    public List<WalletEfCore> WalletsEfCore { get; init; } = [];
 }
