7ebe27f497e09e33d14c773febedb472cd1fd5f9
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 00:50:00 +0500

    #10
    1) Пофиксил, у одного кошелька может быть много пользователей

--- Back_PaymentForm/PaymentForm.Core/Abstractions/IRepositories/IWalletRepository.cs
+++ Back_PaymentForm/PaymentForm.Core/Abstractions/IRepositories/IWalletRepository.cs
@@ -5,7 +5,7 @@ namespace PaymentForm.Core.Abstractions.IRepositories;
 public interface IWalletRepository
 {
     Task<IEnumerable<Wallet>> GetAll();
+    Task<IEnumerable<Wallet>> GetByUserId(long userId);
     Task<Wallet?> GetById(long id);
     Task<Wallet?> GetWalletNumber(string walletNumber);
-    Task<Wallet?> GetByUserId(long userId);
 }
