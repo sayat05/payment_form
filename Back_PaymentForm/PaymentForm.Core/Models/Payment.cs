9343ef3deeb1994016b9841e3d0c1c3c4c295de6
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-20 20:14:31 +0500

    #10
    1) Исправил иоднль платежа, что бы в нем  хранить id кошелька

--- Back_PaymentForm/PaymentForm.Core/Models/Payment.cs
+++ Back_PaymentForm/PaymentForm.Core/Models/Payment.cs
@@ -5,11 +5,11 @@ namespace PaymentForm.Core.Models;
 public class Payment
 {
     public long Id { get; init; }
-    public long UserId { get; init; }
+    public long WalletId { get; init; }
     public required string Email { get; init; }
     public decimal Amount { get; init; }
     public CurrencyType Currency { get; init; }
     public PaymentStatus Status { get; init; }
     public string Comment { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; init; }
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
 }
