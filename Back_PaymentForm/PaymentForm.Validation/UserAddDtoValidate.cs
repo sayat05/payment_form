d8537617cc6e6f1a4c62406709f67060973d6812
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 13:39:19 +0500

    #10
    1) Пофиксил на проблы

--- Back_PaymentForm/PaymentForm.Validation/UserAddDtoValidate.cs
+++ Back_PaymentForm/PaymentForm.Validation/UserAddDtoValidate.cs
@@ -8,15 +8,21 @@ public class UserAddDtoValidate : AbstractValidator<UserAddDto>
     public UserAddDtoValidate()
     {
         RuleFor(u => u.Name)
+            .Must(n => n == n.Trim())
+            .WithMessage("Must without spaces")
             .MinimumLength(2)
             .MaximumLength(255)
             .NotEmpty();
 
         RuleFor(u => u.Email)
+            .Must(e => e == e.Trim())
+            .WithMessage("Must without spaces")
             .EmailAddress()
             .NotEmpty();
-        
-        RuleFor(u => u.PhoneNumber.Trim())
+
+        RuleFor(u => u.PhoneNumber)
+            .Must(p => p == p.Trim())
+            .WithMessage("Must without spaces")
             .Matches(@"^\+?\d{10,15}$")
             .WithMessage("Phone number must be valid (10-15 digits, optionally start with +).")
             .MinimumLength(10).WithMessage("Phone number is too short.")
