b210c15ff1177797a83464c0b950561c9ef342f1
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 20:07:46 +0500

    1) Добавил форму платежа

--- Front_PaymentForm/src/App.vue
+++ Front_PaymentForm/src/App.vue
@@ -1,25 +1,96 @@
 <template>
-    <div class="h-100 bg-gray-500">
-        <div v-for="currency in enumStore.currenciesType" :key="currency">
-            {{currency}}
-        </div>
+    <div class="bg-gray-50 h-screen">
+        
+        <form @submit.prevent="paymentStore.add" class="border p-3 w-100 rounded border-gray-200 flex flex-col gap-5">
+            <h3 class="text-xl text-center">Payment form</h3>
+            
+            <div class="flex items-center justify-between">
+                <label>Wallet number: </label>
+                <my-input v-model="paymentStore.payment.walletNumber"
+                          placeholder="wallet number"
+                          type="text"
+                          minlength="4"
+                          required/>
+            </div>
+            
+            <div class="flex items-center justify-between">
+                <label>User id: </label>
+                <my-input v-model="paymentStore.payment.userId"
+                          placeholder="user id"
+                          type="number"
+                          required/>
+            </div>
+            
+            <div class="flex items-center justify-between">
+                <label>Email: </label>
+                <my-input v-model="paymentStore.payment.email"
+                          placeholder="email"
+                          type="email"
+                          required/>
+            </div>
+            
+            
+            <div class="flex items-center justify-between">
+                <label>Phone number: </label>
+                <my-input v-model="paymentStore.payment.phoneNumber"
+                          placeholder="phone number"
+                          type="tel"/>
+            </div>
+            
+            <div class="flex items-center justify-between">
+                <label>Currency: </label>
+                <my-select v-model="paymentStore.payment.currency"
+                           name="currency"
+                           :items="enumStore.currenciesType"
+                           required/>
+            </div>
+            
+            <div class="flex items-center justify-between">
+                <label>Payment amount: </label>
+                <my-input v-model="paymentStore.payment.amount"
+                          placeholder="payment amount"
+                          type="number" required/>
+            </div>
+            
+            <div class="flex items-center justify-between">
+                <label>Comment: </label>
+                <my-input v-model="paymentStore.payment.comment"
+                          placeholder="comment"
+                          type="text"/>
+            </div>
+            
+            <my-button class="!mt-3 self-end" type="submit">Pay</my-button>
+        </form>
+    
     </div>
+
 </template>
 
 <script>
+import MyButton from "@/components/MyUI/MyButton.vue";
+import MyInput from "@/components/MyUI/MyInput.vue";
 import {useEnumStore} from "@/pinies/enumStore.js";
+import {useWalletStore} from "@/pinies/walletStore.js";
+import {usePaymentStore} from "@/pinies/paymentStore.js";
+import MySelect from "@/components/MyUI/MySelect.vue";
 
 export default {
     name: 'App',
+    components: {
+        MySelect,
+        MyInput,
+        MyButton
+    },
     data() {
         return {
-            enumStore: useEnumStore()
+            enumStore: useEnumStore(),
+            walletStore: useWalletStore(),
+            paymentStore: usePaymentStore()
         };
     },
-    methods: {
-    },
+    methods: {},
     mounted() {
-       this.enumStore.getCurrenciesType();
+        this.enumStore.getCurrenciesType();
     }
 }
 </script>
