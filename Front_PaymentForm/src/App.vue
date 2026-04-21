f9febdc6ae60fe18ef5eec5c452de3de1d60345f
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 21:15:46 +0500

    #17
    1) Добавил возможность перехода на страницы через пуши

--- Front_PaymentForm/src/App.vue
+++ Front_PaymentForm/src/App.vue
@@ -1,96 +1,31 @@
 <template>
-    <div class="bg-gray-50 h-screen">
+    <div>
+        <h1 class="text-3xl text-center !m-5"> Дефолтная страница</h1>
+        <router-view/>
         
-        <form @submit.prevent="paymentStore.add" class="border p-3 w-100 rounded border-gray-200 flex flex-col gap-5">
-            <h3 class="text-xl text-center">Payment form</h3>
-            
-            <div class="flex items-center justify-between">
-                <label>Wallet number: </label>
-                <my-input v-model="paymentStore.payment.walletNumber"
-                          placeholder="wallet number"
-                          type="text"
-                          minlength="4"
-                          required/>
-            </div>
-            
-            <div class="flex items-center justify-between">
-                <label>User id: </label>
-                <my-input v-model="paymentStore.payment.userId"
-                          placeholder="user id"
-                          type="number"
-                          required/>
-            </div>
-            
-            <div class="flex items-center justify-between">
-                <label>Email: </label>
-                <my-input v-model="paymentStore.payment.email"
-                          placeholder="email"
-                          type="email"
-                          required/>
-            </div>
-            
-            
-            <div class="flex items-center justify-between">
-                <label>Phone number: </label>
-                <my-input v-model="paymentStore.payment.phoneNumber"
-                          placeholder="phone number"
-                          type="tel"/>
-            </div>
-            
-            <div class="flex items-center justify-between">
-                <label>Currency: </label>
-                <my-select v-model="paymentStore.payment.currency"
-                           name="currency"
-                           :items="enumStore.currenciesType"
-                           required/>
-            </div>
-            
-            <div class="flex items-center justify-between">
-                <label>Payment amount: </label>
-                <my-input v-model="paymentStore.payment.amount"
-                          placeholder="payment amount"
-                          type="number" required/>
-            </div>
-            
-            <div class="flex items-center justify-between">
-                <label>Comment: </label>
-                <my-input v-model="paymentStore.payment.comment"
-                          placeholder="comment"
-                          type="text"/>
-            </div>
-            
-            <my-button class="!mt-3 self-end" type="submit">Pay</my-button>
-        </form>
-    
+        <div class="flex flex-col justify-center items-center gap-5 border rounded p-3 w-100 !mx-auto">
+            <my-button @click="toPayments">Платежи</my-button>
+            <my-button @click="toUsers">Пользователи</my-button>
+            <my-button @click="toWallets">Счета</my-button>
+        </div>
     </div>
 
 </template>
 
 <script>
-import MyButton from "@/components/MyUI/MyButton.vue";
-import MyInput from "@/components/MyUI/MyInput.vue";
-import {useEnumStore} from "@/pinies/enumStore.js";
-import {useWalletStore} from "@/pinies/walletStore.js";
-import {usePaymentStore} from "@/pinies/paymentStore.js";
-import MySelect from "@/components/MyUI/MySelect.vue";
-
+import router from "@/myRouter.js";
 export default {
     name: 'App',
-    components: {
-        MySelect,
-        MyInput,
-        MyButton
-    },
-    data() {
-        return {
-            enumStore: useEnumStore(),
-            walletStore: useWalletStore(),
-            paymentStore: usePaymentStore()
-        };
-    },
-    methods: {},
-    mounted() {
-        this.enumStore.getCurrenciesType();
+    methods: {
+        toPayments(){
+            router.push('/payments')
+        },
+        toUsers(){
+            router.push('/users')
+        },
+        toWallets(){
+            router.push('/wallets')
+        }
     }
 }
 </script>
