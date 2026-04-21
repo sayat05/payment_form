4a37e24d322f17ec6cfd41027b1d6a0735c3d883
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 18:58:47 +0500

    #15
    1) Добавил кастомные компоненты что не писать одно и тоже по сто раз одни и те же стили

---
+++ Front_PaymentForm/src/components/MyUI/MyButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="border p-3 cursor-pointer outline-none rounded">
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+    name: "MyButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Front_PaymentForm/src/components/MyUI/MyInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <input :value="modelValue"
+           @input="updateInput"
+           type="text"
+           class="p-2 border border-gray-200 outline-none rounded">
+</template>
+
+<script>
+export default {
+    name: 'MyInput',
+    props: {
+        modelValue: [String, Number],
+    },
+    methods: {
+        updateInput(newVal) {
+            this.$emit('update:modelValue', newVal.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
