735d6d77a600cafd1aab33f054c07393740e6970
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 20:31:55 +0500

    #9
    1) Добавил глобальную регистрацию компонентов которых буду использовать много

---
+++ Front_PaymentForm/src/components/myUI/myUI.js
@@ -0,0 +1,9 @@
+import MyInput from "@/components/myUI/MyInput.vue";
+import MyButton from "@/components/myUI/MyButton.vue";
+import MySelect from "@/components/myUI/MySelect.vue";
+
+export default [
+    MyInput,
+    MyButton,
+    MySelect
+];
--- Front_PaymentForm/src/main.js
+++ Front_PaymentForm/src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css';
 import {createApp} from 'vue';
 import App from './App.vue';
 import {createPinia} from "pinia";
+import components from '@/components/myUI/myUI.js';
 
 const pinia = createPinia();
 
 const app = createApp(App);
 app.use(pinia);
+components.forEach(component => {
+    app.component(component.name, component)
+});
 app.mount('#app');
