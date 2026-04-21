6975ab1d11ee349c4b96d56f3827dbf3bad3cdde
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 20:47:55 +0500

    #17
    1) Добавил страницы сущностей

---
+++ Front_PaymentForm/src/pages/PaymentPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "PaymentPage"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ Front_PaymentForm/src/pages/UserPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: "UserPage"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
