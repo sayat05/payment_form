baeea11f1d1f6ca05a19e521bdb80c46ec56851a
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 18:31:05 +0500

    #13
    1) Установил tailwind

--- Front_PaymentForm/vite.config.js
+++ Front_PaymentForm/vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url';
+import tailwindcss from '@tailwindcss/vite'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        tailwindcss(),
+        vue(),
+        vueDevTools(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-})
+});
