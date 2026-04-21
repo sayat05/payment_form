a299ebec34cb77fffb9d71c95c22bb7793aec9a3
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 23:27:52 +0500

    #17
    1) Реализовал страницу

--- Front_PaymentForm/src/pages/payments/PaymentGetByIdPage.vue
+++ Front_PaymentForm/src/pages/payments/PaymentGetByIdPage.vue
@@ -1,10 +1,41 @@
 <template>
-    Страница payment bt id
+    <form @submit.prevent="getById" class="border rounded w-100 p-3 flex flex-col gap-5 !mx-auto">
+        <div class="flex items-center justify-between">
+            <label>Payment id:</label>
+            <my-input v-model="paymentId" type="number" placeholder="payment id"/>
+        </div>
+        
+        <my-button class="self-end">Get</my-button>
+    </form>
+    
+    <div v-if="isPaymentCard">
+        <payment-card :payment="paymentStore.payment" class="!mx-auto"/>
+    </div>
+
 </template>
 
 <script>
+import {usePaymentStore} from "@/pinies/paymentStore.js";
+import PaymentCard from "@/components/PaymentCard.vue";
+
 export default {
-    name: "PaymentGetByIdPage"
+    name: "PaymentGetByIdPage",
+    components: {
+        PaymentCard
+    },
+    data() {
+        return {
+            paymentStore: usePaymentStore(),
+            paymentId: '',
+            isPaymentCard: false
+        }
+    },
+    methods: {
+        getById() {
+            this.paymentStore.getById(this.paymentId);
+            this.isPaymentCard = true;
+        }
+    }
 }
 </script>
 
