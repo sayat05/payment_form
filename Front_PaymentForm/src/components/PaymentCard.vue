1f4bbb8204a0b154b1ca702aefd5cc49af7d9566
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 20:42:26 +0500

    #15
    1) Добавил карточку payment для множественного переиспользования

---
+++ Front_PaymentForm/src/components/PaymentCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="border !m-3 p-3 w-100">
+        <p>Id: {{payment.Id}}</p>
+        <p>Wallet id: {{payment.walletId}}</p>
+        <p>User email: {{payment.email}}</p>
+        <p>Phone number: {{payment.phoneNumber}}</p>
+        <p>Amount: {{ payment.amount}}</p>
+        <p>Currency: {{payment.currency}}</p>
+        <p>Status: {{payment.status}}</p>
+        <p>Comment: {{payment.comment}}</p>
+        <p>Created at: {{payment.createdAt}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PaymentCard",
+    props: {
+        payment: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
