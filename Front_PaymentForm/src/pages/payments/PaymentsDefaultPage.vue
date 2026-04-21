2c9fb838476e18b29f3d826390240c004791b04f
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 23:28:30 +0500

    #9
    1) Вынес в отдельные страницы

--- Front_PaymentForm/src/pages/payments/PaymentsDefaultPage.vue
+++ Front_PaymentForm/src/pages/payments/PaymentsDefaultPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-start gap-3 border w-200 rounded p-3">
+    <div class="flex flex-col items-start gap-3 border w-200 rounded p-3 !mx-auto">
         <router-link to="/payments/add">
             <my-button>Совершить платеж</my-button>
         </router-link>
@@ -8,19 +8,11 @@
             <my-button>Посмотреть платеж по id</my-button>
         </router-link>
         
-<!--        <my-button v-if="!isGetById" @click="isGetByIdForm = !isGetByIdForm">Посмотреть платеж по id</my-button>-->
-<!--        <div v-else>-->
-<!--            <my-button @click="isGetById = false">Скрыть платеж по id</my-button>-->
-<!--            <form v-if="isGetByIdForm" @submit.prevent="getById">-->
-<!--                <label>Payment id: </label>-->
-<!--                <my-input v-model="paymentId" placeholed="payment id" type="number"/>-->
-<!--            </form>-->
-<!--        </div>-->
+        <router-link to="/payments/stats">
+            <my-button>Посмотреть статистику</my-button>
+        </router-link>
         
         
-        <my-button>Посмотреть сумму платежей за один день</my-button>
-        <my-button>Посмотреть сумму за все платежи</my-button>
-        <my-button>Посмотреть количество платежей за один день</my-button>
         
         <my-button v-if="!isGetAll" @click="getAll">Посмотреть все платежи</my-button>
         <my-button v-else @click="isGetAll = false">Скрыть все платежи</my-button>
@@ -35,9 +27,6 @@
                       :payment="payment"/>
     </div>
     
-    <div v-if="isGetByIdCard">
-        <payment-card :payment="paymentStore.payment"/>
-    </div>
 </template>
 
 <script>
@@ -57,25 +46,12 @@ export default {
             isGetAll: false,
             isGetCreated: false,
             isGetRejected: false,
-            isGetById: false,
-            isCountByDay: false,
-            isSumByDay: false,
-            isTotalSum: false,
-            isGetByIdForm: false,
-            
-            paymentId: ''
-            
         }
     },
     methods: {
         async getAll() {
-            
             this.isGetAll = true;
             await this.paymentStore.getAll();
-        },
-        async getById() {
-            this.isGetById = true;
-            this.paymentStore.getById(this.paymentId);
         }
     }
 }
