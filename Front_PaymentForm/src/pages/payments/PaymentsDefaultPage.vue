e91ba942dd4d18d7f1992860dd70963def1012b6
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-22 00:05:41 +0500

    1) Завершил с payments

--- Front_PaymentForm/src/pages/payments/PaymentsDefaultPage.vue
+++ Front_PaymentForm/src/pages/payments/PaymentsDefaultPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-start gap-3 border w-200 rounded p-3 !mx-auto">
+    <div class="flex flex-col items-start gap-5 border w-200 rounded p-3 !mx-auto">
         <router-link to="/payments/add">
             <my-button>Совершить платеж</my-button>
         </router-link>
@@ -13,31 +13,43 @@
         </router-link>
         
         
-        
         <my-button v-if="!isGetAll" @click="getAll">Посмотреть все платежи</my-button>
         <my-button v-else @click="isGetAll = false">Скрыть все платежи</my-button>
         
-        <my-button>Посмотреть все созданные платежи</my-button>
-        <my-button>Посмотреть все отклоненные платежи</my-button>
+        <my-button v-if="!isGetCreated" @click="getCreated">Посмотреть все созданные платежи</my-button>
+        <my-button v-else @click="isGetCreated = false">Скрыть все созданные платежи</my-button>
+        
+        <my-button v-if="!isGetRejected" @click="getRejected">Посмотреть все отклоненные платежи</my-button>
+        <my-button v-else @click="isGetRejected = false">Скрыть все отклоненные платежи</my-button>
     </div>
     
     <div v-if="isGetAll">
+        <p class="text-center !mt-10">Все платежи</p>
         <payment-card v-for="payment in paymentStore.payments"
                       :key="payment.id"
-                      :payment="payment"/>
+                      :payment="payment"
+                      class="!mx-auto"/>
     </div>
     
+    <div v-if="isGetCreated">
+        <p class="text-center !mt-5">Количество созданных платежей: {{ paymentStore.payments.length }}</p>
+        <payment-card v-for="payment in paymentStore.payments" :key="payment.id" :payment="payment" class="!mx-auto"/>
+    </div>
+    
+    <div v-if="isGetRejected">
+        <p class="text-center !mt-5">Количество отклоненных платежей: {{ paymentStore.payments.length }}</p>
+        <payment-card v-for="payment in paymentStore.payments" :key="payment.id" :payment="payment" class="!mx-auto"/>
+    </div>
+
 </template>
 
 <script>
 import PaymentCard from "@/components/PaymentCard.vue";
 import {usePaymentStore} from "@/pinies/paymentStore.js";
-import MyInput from "@/components/myUI/MyInput.vue";
 
 export default {
     name: "PaymentsDefaultPage",
     components: {
-        MyInput,
         PaymentCard
     },
     data() {
@@ -52,6 +64,14 @@ export default {
         async getAll() {
             this.isGetAll = true;
             await this.paymentStore.getAll();
+        },
+        async getCreated() {
+            this.isGetCreated = true;
+            await this.paymentStore.getCreated();
+        },
+        async getRejected() {
+            this.isGetRejected = true;
+            await this.paymentStore.getRejected();
         }
     }
 }
