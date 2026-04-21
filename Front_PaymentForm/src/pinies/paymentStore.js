28d615e609bcb8c5f5eac3cfa438423767585276
Sayat Raximbekov <saintaccaunt@gmail.com>
2026-04-21 20:19:41 +0500

    #16
    1) Добавил ве методы payments

--- Front_PaymentForm/src/pinies/paymentStore.js
+++ Front_PaymentForm/src/pinies/paymentStore.js
@@ -3,19 +3,82 @@ import axios from "@/api.js";
 
 export const usePaymentStore = defineStore('payment', {
     state: () => ({
-        payment:{},
+        payment: {},
         payments: [],
+        sum: null,
+        count: null
 
     }),
     actions: {
-
-        async add(){
-            try{
+        async getAll() {
+            try {
+                const response = await axios.get('payments');
+                this.payments = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async getCreated() {
+            try {
+                const response = await axios.get('payments/getCreated');
+                this.payments = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async getRejected() {
+            try {
+                const response = await axios.get('payments/getRejected');
+                this.payments = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async getById(id) {
+            try {
+                const response = await axios.get(`payments?id=${id}`);
+                this.payment = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async getSumByDay(dateTime) {
+            try {
+                const response = await axios.get('payments/getSumByDay', dateTime);
+                this.sum = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async getCountByDay() {
+            try {
+                const response = await axios.get('payments/');
+                this.count = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async geTotalSum() {
+            try {
+                const response = await axios.get('payments/');
+                this.sum = response.data;
+            } catch (error) {
+                console.log(error.response);
+                alert(error.message);
+            }
+        },
+        async add() {
+            try {
                 const response = await axios.post('payments/add', this.payment);
                 alert(`Id: ${response.data.id} \nStatus: ${response.data.status}`);
-                this.paymment = {};
-
-            } catch (error){
+                this.payment = {};
+            } catch (error) {
                 console.log(error.response);
                 alert(error.message);
             }
